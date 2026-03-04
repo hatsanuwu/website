1e47c1ef6b168f3408422060619e4acc35162b33
Fragoso <l21070009@huetamo.tecnm.mx>
2026-03-04 17:55:21 -0600

    Make contact form submit handler async
    Updated contact form submit event handler to be asynchronous.

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const contactForm = document.getElementById('contact-form');
 
     if (contactForm) {
-        contactForm.addEventListener('submit', function(e) {
+        contactForm.addEventListener('submit', async function(e) {
             e.preventDefault();
 
             const nameInput = this.querySelector('#name');
